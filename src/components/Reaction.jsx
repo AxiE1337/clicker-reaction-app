c41724480e592ee8f099a09dbc4386636bacefc5
AxiE1337 <pixell390@gmail.com>
2021-10-20 15:07:20 +0300

    1.0

--- src/components/Reaction.jsx
+++ src/components/Reaction.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useRef} from 'react'
+import React,{useState, useRef, useEffect} from 'react'
 import './styles/Reaction.css'
 import { motion } from 'framer-motion'
 
@@ -8,41 +8,40 @@ function Reaction() {
     const [box, setBox] = useState(false)
     const [lose, setLose] = useState(false)
     const [end, setEnd] = useState(true)
+    const [myTimer, setMyTimer] = useState(null)
+    const [randTime, setRandTime] = useState(null)
     const timer = useRef(0)
-    const randNum = Math.floor(Math.random() * (100))*50
+    const randNum = Math.floor(Math.random() * (80) + 20)*50
     const activeBox = [box?'reaction-box-active':'reaction-box']
 
     function reactionBox () {
+        timer.current = 0
         setBtn(false)
         setLose(false)
         setEnd(false)
         setBox(false)
-        setTimeout(() => {
-            const myTimer = setInterval(() => {
+        setRandTime(setTimeout(() => {
+            setMyTimer(setInterval(() => {
                 setBox(true)
                 timer.current += 10
-                if (timer.current > 1500) {
-                    clearInterval(myTimer)
-                    timer.current = 0
-                    setBox(false)
+                if (timer.current > 2000) {
                     setEnd(true)
-                    setTimeout(() => {
-                        timer.current = 0
-                        setBtn(true)
-                    }, 1000)
-                } 
-            }, 10)
-        }, randNum)
+                    setBtn(true)
+                }
+            }, 10))
+        }, randNum))
     }
-
+    
     function reaction () {
+        clearInterval(myTimer)
+        clearTimeout(randTime)
         if (!box) {
             timer.current = 0
             setEnd(true)
             setLose(true)
             setTimeout(() => {
-                timer.current = 0
                 setLose(false)
+                setBtn(true)
             }, 2500)
         } else {
             setBox(false)
@@ -51,19 +50,26 @@ function Reaction() {
                 setBtn(true)
             }, 2000)
         }
-
     }
+
+    useEffect(() => {
+        if (timer.current > 1990) {
+            timer.current = 0
+            clearInterval(myTimer)
+        }
+    }, [end, box, myTimer])
 
     return (
         <motion.div initial={{scaleY: 0}} animate={{scaleY: 1}} exit={{scaleY: 0}}>
-            <div className='reaction-component'>
+            <motion.div whileTap={{}} className='reaction-component'>
                 <div className='reaction'>
-                    {lose&&<h1>Wait for a green quad</h1>}
+                    {lose&&<h1>Wait for a green square</h1>}
                     {end && <h1>Reaction time {timer.current+' ms'}</h1>}
                     {!end && <div onClick={reaction} className={activeBox}></div>}
-                    {btn? <button onClick={reactionBox} className='start-btn'>Start</button>:''}
+                    {btn? <motion.button whileHover={{scale: 1.05}}
+                        onClick={reactionBox} className='start-btn'>Start</motion.button>:''}  
                 </div>
-            </div>
+            </motion.div>
         </motion.div>   
     )
 }
